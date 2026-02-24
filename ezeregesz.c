c95c8456b84452c620e2824ef2c80ae3e31aa78f
rekaszcs <rekaszucs20@gmail.com>
2026-02-24 12:13:31 +0100

    Add files via upload

---
+++ ezeregesz.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1;i < 1000; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            printf("%d\n", i);
+        }
+    }
+        
+    return 0;
+}
