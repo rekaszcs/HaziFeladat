f1a207d735925167fc1c7ecaf47f2a91fbd6ef5a
rekaszcs <rekaszucs20@gmail.com>
2026-03-26 23:59:55 +0100

    Add files via upload

---
+++ happy_new_year.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char a = '<';
+    char b = '"';
+
+    char javitaselso = '.';
+    char javitasketto = ' ';
+
+    printf("%d\n", (a * b) - (javitaselso - javitasketto));
+}
