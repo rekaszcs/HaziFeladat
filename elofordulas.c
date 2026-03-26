224974dd94f8402ce0d09ba48fc8b590d5c0d435
rekaszcs <rekaszucs20@gmail.com>
2026-03-26 22:49:41 +0100

    elofordulas.c

--- elofordulas.c
+++ elofordulas.c
@@ -1,7 +1,8 @@
+#include "prog1.h"
 #include <stdio.h>
 #include <string.h>
 
-int rfind_char(char *s, char c) {
+int rfind_char(string s, char c) {
     int hossz = strlen(s);
 
     for (int i = hossz -1; i >= 0; i--){
